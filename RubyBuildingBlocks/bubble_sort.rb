6ae2b4f2d4d0b085d3706265dd569c1e206753e2
KSheehan <ksheehan927@gmail.com>
2017-02-03 17:36:46 -0500

    Refactor bubble sort algorithm, new blog post about bubble sort

--- RubyBuildingBlocks/bubble_sort.rb
+++ RubyBuildingBlocks/bubble_sort.rb
@@ -1,38 +1,45 @@
 def bubble_sort(array)
-  swap_counter = -1
+  flag = true
 
-  array.each_with_index do |index, x|
-    array.each_with_index do |index, y|
-      swap_counter == 0
+  array.each_with_index do |out_index, x|
+    array.each_with_index do |in_index, y|
       if array[y] > array[x]
         array[x], array[y] = array[y], array[x]
-        swap_counter += 1
-      else
-        swap_counter += 1
+        flag = false
       end
-      break if swap_counter == 0
+      break if flag = true
     end
   end
+    p flag
+    p array
 end
 
 def bubble_sort_by(array)
-        swap_counter = -1
+        flag = true
 
   array.each_with_index do |index, x|
     array.each_with_index do |index, y|
-      swap_counter == 0
       if (yield array[x], array[y]) > 0
          array[x], array[y] = array[y], array[x]
-         swap_counter += 1
-      else
-        swap_counter += 1
+         flag = false
       end
-      break if swap_counter == 0
+      break if flag = false
     end
   end
+    p flag
+    p array
 end
 
 bubble_sort_by(["hi","hey","hello"]) do |left,right|
        left.length - right.length
   end
 bubble_sort_by(["a", "b", "c"]) { |a, b| a <=> b }
+
+
+
+
+
+
+
+
+
