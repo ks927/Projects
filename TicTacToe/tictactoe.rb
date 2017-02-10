48969ebdcbf04a95a9224f00a5b0953ad9640c64
KSheehan <ksheehan927@gmail.com>
2017-02-10 08:15:38 -0500

    complete tic tac toe game

--- TicTacToe/tictactoe.rb
+++ TicTacToe/tictactoe.rb
@@ -20,7 +20,7 @@ class Board
     end
     
     def change_turn
-       @current_turn = @current_turn == "X" ? "O" : "X" 
+       @current_turn = @current_turn == "X" ? "O" : "X" unless @game_over == true
     end
     
     def player_move(index)
@@ -30,49 +30,60 @@ class Board
             else
                 @board[index] = @current_turn
                 show_board
+                check_for_winner
                 change_turn
             end
     end
     
-    
-    def declare_winner
+    private
+    def check_for_winner
         if @board[0] == @board[4] && @board[4] == @board[8]
-           game_over = true
+           @game_over = true
+            puts "Three in a row! #{@current_turn} has won!"
         elsif @board[2] == @board[4] && @board[4] == @board[6]
-            game_over = true
+            @game_over = true
+            puts "Three in a row! #{@current_turn} has won!"
         elsif @board[0] == @board[1] && @board[1] == @board[2]
-            game_over = true
+            @game_over = true
+            puts "Three in a row! #{@current_turn} has won!"
         elsif @board[3] == @board[4] && @board[4] == @board[5]
-            game_over = true
+            @game_over = true
+            puts "Three in a row! #{@current_turn} has won!"
         elsif @board[6] == @board[7] && @board[7] == @board[8]
-            game_over = true
+            @game_over = true
+            puts "Three in a row! #{@current_turn} has won!"
         elsif @board[0] == @board[3] && @board[3] == @board[6]
-            game_over = true
+            @game_over = true
+            puts "Three in a row! #{@current_turn} has won!"
         elsif @board[1] == @board[4] && @board[4] == @board[7]
-            game_over = true
+            @game_over = true
+            puts "Three in a row! #{@current_turn} has won!"
         elsif @board[2] == @board[5] && @board[5] == @board[8]
-            game_over = true
+            @game_over = true
+            puts "Three in a row! #{@current_turn} has won!"
         else
-            game_over = false
+            @game_over = false
         end
     end
   
 end
 
-winners = [[0, 4, 8], [2, 4, 6], [0, 1, 2], [3, 4, 5],
-             [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8]]
+#Pseudocode
 
-x = Board.new
-# initializes "X" and "Y" and board
-x.player_move(4)
+# winners = [[0, 4, 8], [2, 4, 6], [0, 1, 2], [3, 4, 5],
+#             [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8]]
+
+# x = Board.new
+# initializes "X" and "O" and board
+# x.player_move(4)
 # places X in middle square
-x.player_move(0)
-# places Y in first square
-x.player_move(3)
-# places X middle left square
-y.player_move(4)
+# x.player_move(0)
+# places O in first square
+# x.player_move(3)
+# places O middle left square
+# y.player_move(4)
 # "Space is already taken"
-x.player_move(5)
+# x.player_move(5)
 # Three in a row "X" wins!
 
 
