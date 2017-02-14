49b43f2c91ad12dbb0851998ca47de1b9acb2a46
KSheehan <ksheehan927@gmail.com>
2017-02-14 18:56:21 -0500

    commit of event_manager CSV project

---
+++ event_manager/lib/event_manager.rb
@@ -0,0 +1,66 @@
+=begin
+puts "EventManager Initialized"
+
+# contents = File.read "event_attendees.csv"
+# puts contents
+puts "true!" if File.exist? "event_attendees.csv"
+
+lines = File.readlines "event_attendees.csv"
+lines.each_with_index do |line, index|
+    next if index == 0
+    columns = line.split(',')
+    name = columns[2]
+    puts name
+end
+=end
+
+require 'csv'
+require 'sunlight/congress'
+require 'erb'
+
+Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
+
+puts "EventManager initialized"
+
+def clean_zipcode(zipcode)
+   zipcode.to_s.rjust(5,"0")[0..4]
+end
+
+def legislators_by_zipcode(zipcode)
+    legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
+
+end
+
+def save_thank_you_letters(id, form_letter)
+    Dir.mkdir("output") unless Dir.exists? "output"
+    
+    filename = "output/thanks_#{id}.html"
+    
+    File.open(filename,'w') do |file|
+        file.puts form_letter
+    end
+end
+
+contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+
+template_letter = File.read "form_letter.erb"
+erb_template = ERB.new template_letter
+
+contents.each do |row|
+    id = row[0]
+    name = row[:first_name]
+    
+    zipcode = clean_zipcode(row[:zipcode])
+    
+    legislators = legislators_by_zipcode(zipcode)
+    
+    form_letter = erb_template.result(binding)
+    
+    save_thank_you_letters(id, form_letter)
+end
+
+
+
+
+
+
