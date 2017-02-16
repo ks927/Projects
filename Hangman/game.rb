c9b0381a876528e0b6fd93cc04ab2c7923de53bb
KSheehan <ksheehan927@gmail.com>
2017-02-16 16:15:36 -0500

    Completed Hangman game

--- Hangman/game.rb
+++ Hangman/game.rb
@@ -7,7 +7,7 @@ class Game
         @incorrect_guesses = []
         generate_word
     end
-    
+    # load dictionary and generate random word between 5 and 12 characters
     def generate_word
         good_words = []
         File.readlines('dictionary.txt').each do |word| 
@@ -16,10 +16,10 @@ class Game
             end
         end
         @secret_word = good_words.sample.strip.downcase
-        @letters = @secret_word.split('').to_s
-        
+        @letters = @secret_word.split('') # separate letters
+        @blanks = Array.new(@secret_word.length, '_') # create blanks to fill in
     end
-    
+    # check for existence and load game with play method
     def load_game
         puts "What is the name of your game?"
         name = gets.chomp
@@ -32,7 +32,7 @@ class Game
         end
             
     end
-    
+    # save game to saved directory
     def save_game
         puts "Enter name for your game: "
         name = gets.chomp
@@ -40,48 +40,49 @@ class Game
         puts "Game saved!"
         Process.exit
     end
-    
+    # prompt user for guesses and check for matches
     def check_guess
-        # prompt user for guesses
-            print "Guess a letter or type 'save' to save game "
+        print "Guess a letter or type 'save' to save game "
             @guess = gets.chomp.downcase
             if @letters.include?@guess
-                @correct_guesses << @guess
                 puts "You got one!"
+                @letters.each_with_index do |letter, index|
+                    @blanks[index] = letter if letter == @guess
+                end
+            elsif @guess == "save"
+                save_game
             else
                 puts "Nice try but guess again!"
                 @incorrect_guesses << @guess
                 @guesses = @guesses -= 1
             end
-        
     end
-
+    # show the updated blanks after each turn
+    def display
+       @blanks.each { |letter| print "#{letter} " } 
+    end
+    # the game loop with all the methods
     def play
         until @game_over == true
-            if @guess == "save"
-                save_game
-            else
                 check_guess
+                display
                 check_end_game
                 puts "#{@correct_guesses}"
                 puts "#{@incorrect_guesses} You have #{@guesses} guesses left!"
-            end
         end    
     end
-    
+    # if guesses run out or blanks get filled in, game_over is set to true, ending the game
     def check_end_game
         if @guesses == 0
             @game_over = true
             print "Game over, you lose!"
             puts "The secret word was #{@secret_word}!"
-        elsif @correct_guesses == @secret_word  
+        elsif @blanks.none? { |letter| letter == '_' }  
                 @game_over = true
                 print "You win!"
                 puts "The secret word is #{@secret_word}"
-        end
-        
+        end 
     end
-    
 end
 
 
@@ -92,7 +93,7 @@ case gets.chomp.upcase
     when "L" then Game.new.load_game
     when "N" then puts "Fine then, maybe another time"
         else
-        puts "not sure"
+        puts "Didn't catch that, try again"
 end
     
 
