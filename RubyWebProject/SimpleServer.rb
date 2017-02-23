461fa3dcf1168e71fe8e2084f6f5c774c15ee95b
KSheehan <ksheehan927@gmail.com>
2017-02-22 19:02:29 -0500

    added POST feature to tiny browser

--- RubyWebProject/SimpleServer.rb
+++ RubyWebProject/SimpleServer.rb
@@ -1,11 +1,18 @@
 require 'socket'
+require 'json'
 
 server = TCPServer.open(8000) # Socket to listen on port 2000
 loop { #Servers run forever
   client = server.accept  # Wait for a client to connect
   method = client.gets.split # Split the request line
     
-    if method[0] == "GET" # first element = GET
+    if method[0] == "POST"
+        hash = method
+        params = JSON.parse(hash)
+    end
+        
+        
+    if method[0] == "GET" # 1st element = GET
         path = method[1].to_s 
         if File.file?(path) # if 2nd elem = our file
             f = File.open(path) # open that file
--- RubyWebProject/TinyWebBrowser.rb
+++ RubyWebProject/TinyWebBrowser.rb
@@ -1,19 +1,44 @@
 require 'socket'
+require 'json'
  
 host = 'localhost'     # The web server
 port = 8000            # Default HTTP port
 path = "index.html"   # The file we want 
+post_path = "thanks.html"
+
+puts "Would you like to GET or POST ?"
+choice = gets.chomp
 
 # This is the HTTP request we send to fetch a file
-request = "GET #{path} HTTP/1.0\r\n\r\n"
-
-
-### THIS IS WHAT THE SOCKET SENDS TO THE SERVER ###
-### AND GETS OUTPUT TO THE TERMINAL ###
-
-socket = TCPSocket.open(host,port)  # Connect to server
+if choice == "GET"
+    request = "GET #{path} HTTP/1.0\r\n\r\n"
+    socket = TCPSocket.open(host,port)  # Connect to server
 socket.print(request)               # Send request
 response = socket.read              # Read complete response
 # Split response at first blank line into headers and body
 headers,body = response.split("\r\n\r\n", 2) 
 print body                          # And display it
+
+elsif choice == "POST"
+    puts "What would you like your Viking name to be?"
+    name = gets.chomp
+    puts "What would you like your Viking email to be?"
+    email = gets.chomp
+    hash = {:viking=> {:name=>name, :email=>email} }
+    info = hash.to_json
+    message = "POST #{post_path} HTTP/1.0\r\n" +
+             "From: blah@yahoo.com\r\n" +
+             "User-Agent: HTTPTool/1.0\r\n" +
+             "Content-Type: application/x-www-form-urlencoded\r\n" +
+             "Content-Length: #{info.size}\r\n\r\n" +
+             "#{hash}\r\n"
+    socket = TCPSocket.open(host,port)
+    socket.print(message)
+    response = socket.read
+    puts "#{message}"
+end
+
+### THIS IS WHAT THE SOCKET SENDS TO THE SERVER ###
+### AND GETS OUTPUT TO THE TERMINAL ###
+
+
