66646493c94b498817046e0fc90bda0975fa93dd
KSheehan <ksheehan927@gmail.com>
2017-02-23 14:09:29 -0500

    completed project all features working

--- RubyWebProject/SimpleServer.rb
+++ RubyWebProject/SimpleServer.rb
@@ -10,14 +10,31 @@ loop { #Servers run forever
     break if header =~ /\r\n\r\n$/ # breaks at end of initial request line
   end
     puts "Request: #{header}"
-    method = header.split # split header
-    if method[0] == "GET" # 1st element = GET
+    method = header.split # split headers
+    content_length = header.split.last.to_i # split headers and retrieve last one
+    viking_hash = client.read(content_length) # store hash in variable
+    if method[0] == "POST" # if first elem is POST
+        path = method[1].to_s # second elem is path
+        if File.exist?(path)
+            params = JSON.parse(viking_hash) # parse the hash
+            new_yield = "<li>Name: #{params['viking']['name']}</li><li>Email: #{params['viking']['email']}</li>"
+            file = File.read(path) # open that file
+            body = file.gsub('<%= yield %>', new_yield) # sub parsed hash in body
+            client.print "HTTP/1.0 200 OK\r\n" +
+                         "Content-Type: text/html\r\n" +
+                         "Content-Length: #{body.bytesize}" +
+                         "Connection: close\r\n"
+            client.print body # print the body
+        end
+            
+    elsif method[0] == "GET" # 1st element = GET
         path = method[1].to_s 
         if File.exist?(path) # if 2nd elem = our file
             file = File.read(path) # open that file
             client.print "HTTP/1.0 200 OK\r\n" +
                     "Content-Type: text/html\r\n" +
-                    "Content-Length: #{file.size}\r\n\r\n"
+                    "Content-Length: #{file.size}\r\n" +
+                    "Connection: close\r\n\r\n"
             client.print file
         else
             message = "File not found"
--- RubyWebProject/TinyWebBrowser.rb
+++ RubyWebProject/TinyWebBrowser.rb
@@ -6,6 +6,7 @@ port = 8000            # Default HTTP port
 path = "index.html"   # The file we want 
 post_path = "thanks.html"
 
+
 puts "Would you like to GET or POST ?"
 choice = gets.chomp
 
@@ -13,30 +14,35 @@ choice = gets.chomp
 if choice == "GET"
     request = "GET #{path} HTTP/1.0\r\n\r\n"
     socket = TCPSocket.open(host,port)  # Connect to server
-socket.print(request)               # Send request
-response = socket.read              # Read complete response
-# Split response at first blank line into headers and body
-headers,body = response.split("\r\n\r\n", 2) 
-print body                          # And display it
-
+    socket.print(request)               # Send request
+    response = socket.read              # Read complete response
+    
+    # Split response at first blank line into headers and body
+    headers,body = response.split("\r\n\r\n", 2)
+    print headers
+    print body                          # And display it
+    
 elsif choice == "POST"
     puts "What would you like your Viking name to be?"
     name = gets.chomp
     puts "What would you like your Viking email to be?"
     email = gets.chomp
-    hash = {:viking=> {:name=>name, :email=>email} }
+    hash = {:viking => {:name=>name, :email=>email} }
     info = hash.to_json
     message = "POST #{post_path} HTTP/1.0\r\n" +
              "From: blah@yahoo.com\r\n" +
              "User-Agent: HTTPTool/1.0\r\n" +
              "Content-Type: application/x-www-form-urlencoded\r\n" +
              "Content-Length: #{info.size}\r\n\r\n" +
-             "#{hash}\r\n"
+             "#{info}\r\n"
     socket = TCPSocket.open(host,port)
     socket.print(message)
     response = socket.read
-    puts "#{message}"
+    headers,body = response.split("\r\n\r\n", 2)
+    print headers
+    print body                          # And display it
 end
+
 
 ### THIS IS WHAT THE SOCKET SENDS TO THE SERVER ###
 ### AND GETS OUTPUT TO THE TERMINAL ###
