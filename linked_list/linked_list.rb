cce0ef4ad47e56949154cb3e0614a57ec3874d27
KSheehan <ksheehan927@gmail.com>
2017-03-06 18:05:44 -0500

    completed linked list project all methods working

--- linked_list/linked_list.rb
+++ linked_list/linked_list.rb
@@ -4,7 +4,7 @@ class LinkedList
     class Node
         attr_accessor :value, :next_node
     
-        def initialize(value=nil, next_node=nil)
+        def initialize(value, next_node=nil)
         @value = value
         @next_node = next_node
         end
@@ -14,21 +14,15 @@ class LinkedList
     
     def initialize(value)
         @head = Node.new(value)
-        @tail = nil
-        @list_size = 0
+        @tail = @head
+        @list_size = 1
     end
     
     def append(value)
         node = Node.new(value)
         @list_size = @list_size += 1
-        if @head.nil? # if head is empty
-            @head = node # make it your node
-            @head = @tail # there is only one node
-        else
-            @tail = node
             @tail.next_node = node # make tail point to node
-            @tail = @tail.next_node
-       end
+            @tail = node # make node last element
     end
     
     def prepend(value)
@@ -38,6 +32,89 @@ class LinkedList
         @head = node # new head = node
     end
     
+    def size
+        return @list_size
+    end
     
+    def head
+       return @head 
+    end
+    
+    def tail
+       return @tail 
+    end
+    
+    def at(index)
+        if index < 0 || index >= @list_size
+            puts "Error: Incorrect index"
+        else
+        node = @head
+        count = 0
+        while count != index
+            node = node.next_node
+            count += 1
+        end
+        return node.value
+        end
+    end
+    
+    def pop
+        current_node = @head
+        current_node = current_node.next_node until current_node.next_node == @tail
+            @tail.value = nil
+            @tail = current_node
+            @tail.next_node = nil
+    end
+        
+    def contains?(value)
+        node = @head
+        count = 0
+        while count != @list_size
+            if node.value == value
+                return true
+            end
+            node = node.next_node
+            count += 1
+        end
+    end
+        
+    def find(data)
+        node = @head
+        count = 0
+        while count != @list_size
+            if node.value == data
+                return count
+            end
+            node = node.next_node
+            count += 1
+        end
+    end
+    
+    def to_s
+        node = @head
+        str = ""
+        count = 0 
+        while count != @list_size
+            str << "( #{node.value} ) -> "
+            node = node.next_node
+            count += 1
+          end
+          str << "nil"
+          str
+    end
+        
+        
 end
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
