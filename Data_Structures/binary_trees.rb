f2e881bcb3411461abb8b0e99b093305c98c3131
KSheehan <ksheehan927@gmail.com>
2017-03-08 21:58:54 -0500

    build_tree function complete for unsorted arrays

--- Data_Structures/binary_trees.rb
+++ Data_Structures/binary_trees.rb
@@ -1,3 +1,4 @@
+class BinarySearch
 
 class Node
     attr_accessor :value, :parent_node, :lchild_node, :rchild_node
@@ -8,31 +9,41 @@ class Node
         @rchild_node = rchild_node
     end
     
-    def build_tree(arr, arr_start=0, arr_end=arr.length-1)
-        return arr if arr_start > arr_end
-        
-        root = arr_start + (arr_end - arr_start)/2
-        puts arr[root]
-        node = Node.new(arr[root])
-        
-        @lchild_node = build_tree(arr, arr_start, root-1)
-        @rchild_node = build_tree(arr, root+1, arr_end)
-        
-        return node
-      
+    def initialize
+       @root = nil 
     end
+    
+    def insert_node(root, leaf)
+    return leaf if root.nil?
+    if leaf.value < root.value && root.lchild_node == nil
+        root.lchild_node = leaf
+    elsif leaf.value > root.value && root.rchild_node == nil
+        root.rchilde_node = leaf
+    elsif leaf.value < root.value && root.lchild_node != nil
+        insert_node(root.lchild_node, leaf)
+    elsif leaf.value > root.value && root.rchild_node != nil
+        insert_node(root.rchild_node, leaf) 
+    elsif leaf.value == root.value && root.rchild_node != nil
+        insert_node(root.rchild_node, leaf) 
+    end
+end
+    
+    def build_tree(arr)
+        @root = Node.new(arr[0])
+        
+        arr.each do |value|
+           leaf = Node.new(value)
+           insert_node(@root, leaf)
+        end
+    end
+    
+end
     
 end
 
 # [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 # [1, 2, 3, 4, 5, 6, 7]
 
-=begin
-def insert_node(root, leaf)
-    return leaf if root.nil?
-    if leaf.value < root.value
-        leaf.value = @lchild_node
-    elsif leaf.value > root.value
-        leaf.value = @rchild_node
+begin
+
 end
-=end
