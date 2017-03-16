e4207702423eb176b3534ef627781f4f6d2e9885
KSheehan <ksheehan927@gmail.com>
2017-03-15 23:53:58 -0400

    Update March page

--- RubyBuildingBlocks/Caesar_Cipher.rb
+++ RubyBuildingBlocks/Caesar_Cipher.rb
@@ -4,13 +4,14 @@ def caesar_cipher(string, num)
     string.each_char do |ch|
       ch = ch.ord
       if ch.between?(97, 122)  || ch.between?(65, 90)
-      ch += (num % 26)
-        if ch > 122 || (ch > 90 && ch < 97)
-          return ch -= 26
-        end
+        ch += (num % 26)
+          if ch > 122 || (ch > 90 && ch < 97)
+            return ch -= 26
+          end
       end
       ch = ch.chr
-      cipher += ch
+      cipher << ch
     end
     puts cipher
+
 end
