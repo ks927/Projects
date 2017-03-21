9a8897ed98cde8bef4328adfdb937582be0938e8
KSheehan <ksheehan927@gmail.com>
2017-03-20 21:21:06 -0400

    remove head and master tags

--- TicTacToe/lib/tictactoe.rb
+++ TicTacToe/lib/tictactoe.rb
@@ -1,17 +1,6 @@
 class Board
-<<<<<<< HEAD:TicTacToe/lib/tictactoe.rb
-<<<<<<< HEAD:TicTacToe/lib/tictactoe.rb
-<<<<<<< HEAD:TicTacToe/tictactoe.rb
+attr_reader :board, :current_turn, :game_over
 
-=======
-attr_reader :board, :current_turn, :game_over
->>>>>>> origin/master:TicTacToe/lib/tictactoe.rb
-=======
-attr_reader :board, :current_turn, :game_over
->>>>>>> origin/master:TicTacToe/lib/tictactoe.rb
-=======
-attr_reader :board, :current_turn, :game_over
->>>>>>> origin/master:TicTacToe/lib/tictactoe.rb
     def initialize
         @board = [
             0, 1, 2,
--- TicTacToe/spec/tic_tac_toe_spec.rb
+++ TicTacToe/spec/tic_tac_toe_spec.rb
@@ -1,54 +1,26 @@
 require "tictactoe"
 
 describe Board do
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 
     before(:all) do
         @game = Board.new
     end
 
-=======
 
-    before(:all) do
-        @game = Board.new
-    end
 
->>>>>>> origin/master
-=======
-
-    before(:all) do
-        @game = Board.new
-    end
-
->>>>>>> origin/master
         describe ".player_move" do
             it "sets the X or O at the proper place on the board" do
                 @game.player_move(8)
                 expect(@game.board[8]). to eq("X")
             end
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-=======
 
->>>>>>> origin/master
-=======
-
->>>>>>> origin/master
             it "changes turn" do
                 expect(@game.current_turn).to eql("O")
             end
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-=======
-
->>>>>>> origin/master
-=======
-
->>>>>>> origin/master
             it "sets game over to true when X or O are in 3 consecutive spots" do
                @game.player_move(0)
                 @game.player_move(5)
@@ -57,36 +29,11 @@ describe Board do
                 @game.player_move(2)
                 expect(@game.game_over).to be true
             end
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 
         end
 
 
-
-
-
-
 end
-=======
-
-        end
 
 
-
-
-
-
-end
->>>>>>> origin/master
-=======
-
-        end
-
-
-
-
-
-
-end
->>>>>>> origin/master
